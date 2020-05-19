c2faa89aa8e216f50a0550929dd1f1b497bf8b7e
anicus19 <anicus19@gmail.com>
2020-05-19 05:43:29 -0700

    Fixed styles on mobile and js

--- sprint-one/src/App.js
+++ sprint-one/src/App.js
@@ -42,7 +42,7 @@ class App extends React.Component {
           "On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title",
       },
       {
-        title: "Become A Travel Pro In One Easy Lesson...",
+        title: "Become A Travel Pro In One Easy Lesson",
         author: "Scotty Cranmer",
         poster: `./asset/image/video-list-1.jpg`,
         media: "./video/Brainstation-Sample-Video.mp4",
@@ -53,7 +53,7 @@ class App extends React.Component {
           "On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title",
       },
       {
-        title: "Les Houches The Hidden Gem Of The...",
+        title: "Les Houches The Hidden Gem Of The Chamonix",
         author: "Scotty Cranmer",
         poster: `./asset/image/video-list-2.jpg`,
         media: "./video/Brainstation-Sample-Video.mp4",
@@ -64,7 +64,7 @@ class App extends React.Component {
           "On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title",
       },
       {
-        title: "Travel Health Useful Medical Information...",
+        title: "Travel Health Useful Medical Information",
         author: "Scotty Cranmer",
         poster: `./asset/image/video-list-3.jpg`,
         media: "./video/Brainstation-Sample-Video.mp4",
--- sprint-one/src/component/Header.js
+++ sprint-one/src/component/Header.js
@@ -2,33 +2,39 @@ import React from "react";
 import logo from "../asset/logo/Logo-brainflix.svg";
 import Mohan from "../asset/image/Mohan-muruge.jpg";
 import UploadIcon from "../asset/icon/SVG/Icon-upload.svg";
+import SearchIcon from "../asset/icon/SVG/Icon-search.svg";
 
 function Header() {
   return (
     <header className="header">
-      <div className="logo-container">
-        <img className="header__logo" src={logo} alt="logo" />
-      </div>
-      <div className="navbar-container">
-        <div className="search-bar-container">
-          <form className="search-bar">
-            <input
-              className="search-bar-input"
-              type="text"
-              name="search"
-              placeholder="Search.."
-            />
-          </form>
+      <div className="header-container">
+        <div className="logo-container">
+          <img className="header__logo" src={logo} alt="logo" />
         </div>
-        <div className="upload-profile-container">
-          <button className="button button-upload">
-            <span>
-              <img src={UploadIcon} />
-            </span>
-            UPLOAD
-          </button>
-          <div className="profile-picture-container">
-            <img className="profile-picture" src={Mohan} alt="profile pic" />
+        <div className="navbar-container">
+          <div className="search-bar-container">
+            <form className="search-bar">
+              <input
+                className="search-bar-input"
+                type="text"
+                name="search"
+                placeholder="Search.."
+              />{" "}
+              <img className="search-icon" src={SearchIcon} />
+            </form>
+          </div>
+          <div className="upload-profile-container">
+            <div className="button-upload-container">
+              <button className="button button-upload">
+                <span>
+                  <img src={UploadIcon} />
+                </span>
+                UPLOAD
+              </button>
+            </div>
+            <div className="profile-picture-container">
+              <img className="profile-picture" src={Mohan} alt="profile pic" />
+            </div>
           </div>
         </div>
       </div>
